253426d819abdcd9fae46f80cc7b1a14857a90c8
“Michellediaz23 <ml7992066@gmail.com>
2024-05-13 07:22:44 +0200

    mi prueba

---
+++ program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more informationusing System;
+using ConsoleApp6;
+using ConsoleApp6;
+
+class Program
+{
+    static void Main()
+    {
+        Universidad miUniversidad = new Universidad();
+
+        // se crearon instancias de alumnos y se  agregaron  a la universidad
+        Alumno alumno1 = new Alumno("Cristobal", 18, "0517U", Alumno.Carreras.Ingeneria_electronica);
+        alumno1.Calificaciones.Add(45);
+        alumno1.Calificaciones.Add(90);
+        Alumno alumno2 = new Alumno("Daniela S", 20, "0719U", Alumno.Carreras.Ingeneria_de_sistemas);
+        alumno2.Calificaciones.Add(24);
+        alumno2.Calificaciones.Add(87);
+        Alumno alumno3 = new Alumno("Andrea  V", 19, "0512U", Alumno.Carreras.Ingeneria_en_computacion);
+        alumno3.Calificaciones.Add(99);
+        alumno3.Calificaciones.Add(98);
+
+        miUniversidad.AgregarAlumno(alumno1);
+        miUniversidad.AgregarAlumno(alumno2);
+        miUniversidad.AgregarAlumno(alumno3);
+        List<Alumno> alumnos = miUniversidad.ObtenerListaAlumno();
+
+        // Mostrar detalles de los alumnos
+        Console.WriteLine("Lista de Alumnos:");
+        Console.WriteLine("┌───────┬───────────┬───────┬───────────┬───────────┐");
+        Console.WriteLine("│ Carnet│   Nombre  │  Prom.│ Estado    │ Detalles  │");
+        Console.WriteLine("├───────┼───────────┼───────┼───────────┼───────────┤");
+
+        foreach (var alumno in alumnos)
+        {
+            Console.WriteLine(value: $"│ {alumno.Carnet} │ {alumno.Nombre} │ {alumno.CalcularPromedioCalificaciones()}  │ {alumno.EstadoAprobacion()}  │");
+        }
+
+        Console.WriteLine("└───────┴───────────┴───────┴───────────┴───────────┘");
+    }
+}
---
+++ universidad.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+
+    public partial class Universidad
+    {
+
+        //Lista de alumnos
+        private List<Alumno> alumnoList;
+
+        //Constructor para inicializar la lista de alumnos
+        public Universidad()
+        {
+            alumnoList = new List<Alumno>();
+        }
+        public void AgregarAlumno(Alumno alumno)
+        {
+            alumnoList.Add(alumno);
+        }
+
+        //metodo para obtener la lista de alumnos
+        public List<Alumno> ObtenerListaAlumno()
+        {
+            return alumnoList;
+        }
+
+    }
+}
